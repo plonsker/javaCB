5afd47cb9bb3fea1fbc03861487d9773757d8f83
joe <joeplonsker@gmail.com>
2016-09-03 14:31:04 -0400

    Solve icy hot

--- front22.java
+++ front22.java
@@ -6,3 +6,16 @@ public String front22(String str) {
   String front = str.substring(0, 2);
   return front + str + front;
 }
+
+// site solution
+
+public String front22(String str) {
+  // First figure the number of chars to take
+  int take = 2;
+  if (take > str.length()) {
+    take = str.length();
+  }
+  
+  String front = str.substring(0, take);
+  return front + str + front;
+}
