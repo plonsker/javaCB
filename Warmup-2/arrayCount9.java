cab8cac7a36e35a74177e7c22b3b017f53b53dca
joe <joeplonsker@gmail.com>
2016-10-08 18:24:25 -0400

    wrap up warmups for Saturday

--- Warmup-2/arrayCount9.java
+++ Warmup-2/arrayCount9.java
@@ -1,8 +1,9 @@
 public int arrayCount9(int[] nums) {
   int count = 0;
   for (int i = 0; i < nums.length; i ++){
-    if (nums[i] == 9)
+    if (nums[i] == 9){
         count++;
+    }
   }
   return count;
 }
